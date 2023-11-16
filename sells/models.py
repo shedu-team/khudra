0f828f53b08f1fa17e642a2415007b00a3505033
Shedu Team <shedu.team@gmail.com>
2023-11-16 20:54:41 +0300

    Product Model added

--- sells/models.py
+++ sells/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.PositiveIntegerField()
+    description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
