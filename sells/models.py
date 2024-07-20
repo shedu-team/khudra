98cf2714e78b60ef12cc72be2244e30a1980bd12
Shedu Team <shedu.team@gmail.com>
2024-07-20 19:39:35 +0300

    freshStart

--- sells/models.py
+++ sells/models.py
@@ -10,3 +10,8 @@ class Products(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.name} _ ({self.price})"
+
+
+
