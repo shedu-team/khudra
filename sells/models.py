242ff7657f35e4265b030d8d6f29ccddddde164f
Shedu Team <shedu.team@gmail.com>
2024-08-12 00:26:07 +0300

    adding tables #2

--- sells/models.py
+++ sells/models.py
@@ -10,7 +10,7 @@ class Product(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.product_name
+        return f"{self.product_name} Price = {self.price}"
 
 # Distributors Table
 class Distributor(models.Model):
